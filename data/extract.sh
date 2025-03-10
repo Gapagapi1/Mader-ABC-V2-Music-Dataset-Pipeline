fc924934cb962da71bad97857379bdbb2dbcb0e3
Julien Zébic <julien.zebic@epita.fr>
2025-03-10 16:55:04 +0100

    feat: add code

---
+++ data/extract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -xvf lmd_matched.tar.gz
---
+++ data/scripts/flatten.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import json
+
+root = "../lmd_matched"
+new_root = "../lmd_matched_flat"
+
+os.makedirs(new_root)
+
+tracks = []
+versions = {}
+
+for root, folders, files in os.walk(root):
+    if len(files) == 0:
+        # print("[NOTHING] root: {} | folders: {} | files: {}".format(root, folders, files))
+        continue
+
+    last_folder = os.path.basename(root)
+    new_last_folder_path = os.path.join(new_root, last_folder)
+    
+    if not os.path.exists(new_last_folder_path):
+        os.makedirs(new_last_folder_path)
+    else:
+        None
+        print("[STRANGE] root: {} | folders: {} | files: {}".format(root, folders, files))
+
+    tracks.append(root.split("/")[-1])
+    
+    for file in files:
+        src_file = os.path.join(root, file)
+        dest_file = os.path.join(new_last_folder_path, file)
+
+        if os.path.exists(dest_file):
+            None
+            print("[MEGA STRANGE] root: {} | folders: {} | files: {}".format(root, folders, files))
+
+        shutil.copy2(src_file, dest_file)
+
+        key = root.split("/")[-1]
+        value = ".".join(dest_file.split(".")[:-1]).split("/")[-1]
+        
+        if key in versions:
+            versions[key].append(value)
+        else:
+            versions[key] = [value]
+        # print("[COPY] root: {} | folders: {} | file: {}".format(root, folders, file))
+
+if True:
+    with open("../tracks.json", "w") as tracks_file:
+        json.dump(tracks, tracks_file)
+    with open("../versions.json", "w") as versions_file:
+        json.dump(versions, versions_file)
