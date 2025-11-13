5fc2eaa043465f017a2e774099251f039591075e
Gapagapi1 <julien.zebic@lesgrandspres.fr>
2025-11-13 19:16:16 +0100

    feat: factorize build_hf_dataset.py

--- scripts/build_hf_dataset.py
+++ scripts/build_hf_dataset.py
@@ -15,7 +15,10 @@ OUT_DIR = Path("./data")
 SHARD_ROWS = 500_000  # adjust based on RAM / size
 WRITE_ABC = True  # set False if you only want tokens
 
-# 9-class mapping
+INSTRUMENTS = ["Piano", "Organ", "Guitar", "Bass", "String", "Vocal", "Brass", "Reed", "Pipe", "Synth Lead", "Synth Pad", "Synth Effect", "Pitched Percussion", "Percussive", "Sound Effect", "Percussion Channel"]
+instrument_label = ClassLabel(names=INSTRUMENTS)
+instrument_to_id = {c:i for i,c in enumerate(INSTRUMENTS)}
+
 GENRES = ["Rock", "Pop", "Electronic", "Country", "RnB", "Jazz", "World", "Other", "Unknown"]
 genre_label = ClassLabel(names=GENRES)
 genre_to_id = {g:i for i,g in enumerate(GENRES)}
@@ -23,11 +26,10 @@ genre_to_id = {g:i for i,g in enumerate(GENRES)}
 VOICE_RE = re.compile(r"voice_(\d+)\.pkl")
 TRACK_RE = re.compile(r"track (\d+)\.abc")
 
-def iter_token_dataset():
+def iter_dataset(root: Path, glob_str: str, compiled_regex, is_abc_text_corpus: bool = True):
     with open(GENRE_JSON, "r", encoding="utf-8") as f:
         msd_to_genre = json.load(f)
-    # Walk {track_id}/{arrangement_id}/voice_*.pkl
-    for track_dir in sorted(TOK_ROOT.iterdir()):
+    for track_dir in sorted(root.iterdir()):
         if not track_dir.is_dir():
             continue
         track_id = track_dir.name
@@ -46,22 +48,30 @@ def iter_token_dataset():
             with open(meta_path, "r", encoding="utf-8") as mf:
                 voice_meta = json.load(mf)
 
-            for pkl_path in sorted(arr_dir.glob("voice_*.pkl")):
-                m = VOICE_RE.search(pkl_path.name)
+            for path in sorted(arr_dir.glob(glob_str)):
+                m = compiled_regex.search(path.name)
                 if not m:
-                    print(f"WARNING: {pkl_path} is not a valid voice pickle name.")
+                    print(f"WARNING: {path} does not exist.")
                     continue
                 voice_idx = int(m.group(1))
                 try:
-                    with open(pkl_path, "rb") as f:
-                        tokens = pickle.load(f)
+                    if is_abc_text_corpus:
+                        content = path.read_text(encoding="utf-8")
+                    else:
+                        with open(path, "rb") as f:
+                             content = [str(t) for t in pickle.load(f)]
                 except Exception:
-                    print(f"WARNING: Could not open '{pkl_path}'.")
+                    print(f"WARNING: Could not read '{path}'.")
                     continue
 
                 vm = voice_meta.get(str(voice_idx))
                 if vm is None:
                     print(f"WARNING: Found invalid voice metadata ({voice_meta}).")
+                    continue
+
+                vi = vm.get("category_name")
+                if vi is None:
+                    print(f"WARNING: Found invalid voice instrument ({vm}).")
                     continue
 
                 yield {
@@ -71,45 +81,10 @@ def iter_token_dataset():
                     "program_id": int(vm["program_id"]),
                     "program_name": vm["program_name"],
                     "category_id": int(vm["category_id"]),
-                    "category_name": vm["category_name"],
+                    "category_name": instrument_to_id[vi],
                     "musescore_info": vm["musescore_info"],
                     "genre": g_id,
-                    "tokens": [str(t) for t in tokens],
-                }
-
-def iter_abc_dataset():
-    with open(GENRE_JSON, "r", encoding="utf-8") as f:
-        msd_to_genre = json.load(f)
-    # Walk {track_id}/{arrangement_id}/track N.abc
-    for track_dir in sorted(ABC_ROOT.iterdir()):
-        if not track_dir.is_dir():
-            continue
-        track_id = track_dir.name
-        genre_name = msd_to_genre.get(track_id)
-        if genre_name is None or genre_name not in genre_to_id:
-            genre_name = "Unknown"
-        g_id = genre_to_id[genre_name]
-        for arr_dir in sorted(track_dir.iterdir()):
-            if not arr_dir.is_dir():
-                continue
-            arrangement_id = arr_dir.name
-            for abc_path in sorted(arr_dir.glob("track *.abc")):
-                m = TRACK_RE.search(abc_path.name)
-                if not m: 
-                    print(f"WARNING: abc_path '{abc_path}' does not exist.")
-                    continue
-                track_num = int(m.group(1))
-                try:
-                    abc_text = abc_path.read_text(encoding="utf-8")
-                except Exception:
-                    print(f"WARNING: Could not read abc file '{abc_path}'.")
-                    continue
-                yield {
-                    "track_id": track_id,
-                    "arrangement_id": arrangement_id,
-                    "track_num": track_num,
-                    "genre": g_id,
-                    "abc": abc_text,
+                    "abc_text" if is_abc_text_corpus else "token_sequence": content,
                 }
 
 def write_parquet_shards(rows_iter, out_path):
@@ -129,9 +104,9 @@ def write_parquet_shards(rows_iter, out_path):
 
 if __name__ == "__main__":
     print("Writing tokens parquet files...")
-    write_parquet_shards(iter_token_dataset(), OUT_DIR / "tokens")
+    write_parquet_shards(iter_dataset(TOK_ROOT, "voice_*.pkl", VOICE_RE, False), OUT_DIR / "abc_tokens")
     if WRITE_ABC:
         print("Writing abc voice parquet files...")
-        write_parquet_shards(iter_abc_dataset(), OUT_DIR / "abc_tracks")
+        write_parquet_shards(iter_dataset(ABC_ROOT, "track *.abc", TRACK_RE, True), OUT_DIR / "abc_texts")
     print("Done. Parquet shards in ./data/")
 
